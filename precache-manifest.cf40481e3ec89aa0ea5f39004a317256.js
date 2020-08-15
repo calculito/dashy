2dcf509dfbe934e0e850a2e94238e4b84c29cd9b
migraTeam <ion.oaie@gmail.com>
2020-08-16 00:51:53 +0200

    Updates

---
+++ precache-manifest.cf40481e3ec89aa0ea5f39004a317256.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce1cc4a5ba3e394ac1e205738452fb59",
+    "url": "/migradashy/index.html"
+  },
+  {
+    "revision": "8ee08ecffe00a9b4c110",
+    "url": "/migradashy/static/css/main.86d3bba4.chunk.css"
+  },
+  {
+    "revision": "369ca98cf63f1ecb6d1e",
+    "url": "/migradashy/static/js/2.d795c7c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/migradashy/static/js/2.d795c7c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ee08ecffe00a9b4c110",
+    "url": "/migradashy/static/js/main.5bf62c44.chunk.js"
+  },
+  {
+    "revision": "2821e758479182671dea",
+    "url": "/migradashy/static/js/runtime-main.a686015d.js"
+  },
+  {
+    "revision": "216f7c0d9e60b331a48f4576a338c11c",
+    "url": "/migradashy/static/media/Screenshot.216f7c0d.png"
+  },
+  {
+    "revision": "dcf38b1f18bb89eb5047d63a7c4a60df",
+    "url": "/migradashy/static/media/syllabus.dcf38b1f.png"
+  }
+]);
