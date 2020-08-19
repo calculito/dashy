8df37e1ac08d0474ea5ac748e404b39e75062728
Jose-cod7 <josegarriaga7@gmail.com>
2020-08-19 03:43:14 +0200

    component links

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,6 +1,50 @@
-import React from "react";
+import React, {useState} from "react";
 
-function Links(i) {
-  return <div className="tabcontent">LINKS</div>;
+
+const Links = () => {
+  const [savedLink, setLinkSaved] = useState([]);
+  const [linkFromInput, setLinkFromInput] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleInputChange = (event) => {
+    setLinkFromInput(event.target.value)
+  }
+
+  const createLink = () => {
+      if (linkFromInput !== "") {
+          setErrorMessage("")
+          setLinkSaved(savedLink.concat(linkFromInput))
+      } else {
+          setErrorMessage("Please enter a link")
+      }
+  };
+
+  
+
+  return (
+      <div id="links">
+          <h3>You have {savedLink.length} links</h3>   
+
+          <input type="text" placeholder="Save your link" value={linkFromInput} onChange={handleInputChange} />
+
+          {errorMessage ? <a>{errorMessage}</a> : null}
+
+          <button onClick={createLink}>Save</button>
+
+          {savedLink.map((link, index) => {
+              return <div>
+                <ul>
+                  <li>
+                    <a href={linkFromInput} target="_blank" key={index}>{link}</a>
+                  </li>
+                </ul>
+             </div>
+              
+          })}         
+          
+      </div>
+  );
 }
+
+
 export default Links;
