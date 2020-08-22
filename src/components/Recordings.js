be6da797ca081528f935d8939c12e54e57ece9da
calculito <ion.oaie@gmail.com>
2020-08-22 10:24:00 +0200

    recordings

--- src/components/Recordings.js
+++ src/components/Recordings.js
@@ -1,6 +1,24 @@
 import React from "react";
 
-function Recordings(i) {
-  return <div className="tabcontent">RECORDINGS (here the links)</div>;
+function Recordings() {
+  const recordingLinks = [
+    "Recording of the class from 01.08.2020",
+    "Recording of the class from 04.08.2020",
+    "Recording of the class from 08.08.2020",
+    "Recording of the class from 11.08.2020",
+  ];
+  return (
+    <div className="tabcontent">
+      {recordingLinks.map((data, i) => (
+        <div className="recordings" key={"d" + i}>
+          <li key={"li" + i}>
+            <button href="" key={"b" + i}>
+              {data}
+            </button>
+          </li>
+        </div>
+      ))}
+    </div>
+  );
 }
 export default Recordings;
